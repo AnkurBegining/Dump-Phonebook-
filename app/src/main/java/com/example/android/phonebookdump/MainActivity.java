33a37be800b41216899ae3f4661e3fdf05785893
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-03-26 16:37:41 +0530

    feat:Changed Static textView and Button to clickable

--- app/src/main/java/com/example/android/phonebookdump/MainActivity.java
+++ app/src/main/java/com/example/android/phonebookdump/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.phonebookdump;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = (TextView) findViewById(R.id.name_text_view);
+
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CallingDetails.class);
+
+                //TODO : We will intent it to calling Activity
+            }
+        });
+
+        Button dial = (Button) findViewById(R.id.call_Button);
+
+        dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO : We will take permission from calling API
+            }
+        });
+
+        TextView number = (TextView) findViewById(R.id.number_text_view);
+
+
     }
 }
